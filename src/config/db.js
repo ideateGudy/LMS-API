285be2fe505bbfb7cf465c9615cbfee119d8d79c
ideateGudy <azonubigoodnews@gmail.com>
2025-04-13 10:11:21 +0100

    Database Connection

---
+++ src/config/db.js
@@ -0,0 +1,15 @@
+import mongoose from "mongoose";
+import { logger } from "../utils/logger.js";
+
+export const connectDb = async () => {
+  try {
+    mongoose.connection.on("connected", () =>
+      logger.info(`✅ DataBase Connected Successfully`)
+    );
+    await mongoose.connect(`${process.env.MONGO_URI}${process.env.DB_NAME}`);
+  } catch (error) {
+    console.error("MongoDB connection error:", error);
+    logger.error("MongoDB connection error:", error);
+    process.exit(1);
+  }
+};
